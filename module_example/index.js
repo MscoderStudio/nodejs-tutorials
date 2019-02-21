4c99cf264b7ecc453ae13b1254ef4de76302d660
snowlab <snowlab@hotmail.com>
2019-02-21 23:46:02 +0800

    模块

--- module_example/index.js
+++ module_example/index.js
@@ -1,9 +1,15 @@
+//https://course.mscoder.cn/nodejs/node-module/
+
 const classModule = require("./moduleClass");
 const eventModule = require("./moduleEvent");
+const example = require("./moduleExample");
 
 var tmpClass = new classModule(1);
-console.log(tmpClass.test());
+console.log("classModule.test():" + tmpClass.test());
 
 eventModule.on("ready", () => {
   console.log("ready事件触发了");
 });
+
+example.testa(1);
+example.testb(2);
